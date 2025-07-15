6094f0f634b1cf03bafd610dbb05a71b25d331f1
brianflann <bflannery7089@gmail.com>
2025-07-15 16:39:06 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -50,7 +50,7 @@ export const managers = [
        "managerID": "337730755755008000",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
        "name": "Brian Flannery",
        //"tookOver": null, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
-       "location": worldwide, // (optional)
+       "location": "worldwide", // (optional)
        "bio": "Her first.",
        "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
