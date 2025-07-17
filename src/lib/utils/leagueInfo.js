e6bc58661429bb8ba949058994af51f1c9d0645a
brianflann <bflannery7089@gmail.com>
2025-07-17 18:41:26 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -32,9 +32,9 @@ export const managers = [
        "managerID": "301226973785518080",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
        "name": "Brian Bisacquino",
        //"tookOver": null, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
-       "location": "PA", // (optional)
-       "bio": "Your reigning champion.",
-       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+       "location": "Maple Glen, PA", // (optional)
+       "bio": "In the rugged, often unforgiving streets of North Wales, a young boy named Bis grew up amidst challenges that would forge a spirit of resilience within him. His early life was a testament to the powerful imagery found in Tupac Shakur's poignant poem, \"The Rose That Grew From the Concrete.\" Like the rose, Bis blossomed against all odds, drawing strength from the very environment that sought to limit him. His intellect and determination, honed by the grit of his surroundings, propelled him towards an extraordinary path. Defying expectations, Bis earned a coveted spot at Bloomsburg University, one of the most prestigious colleges in the United States. There, he not only excelled academically but also broadened his horizons, proving that talent and tenacity know no geographical bounds. Life continued to unfold with remarkable turns. Just last year, Bis showcased his indomitable spirit in a different arena: fantasy football. His team, facing an abysmal 0-5 start, seemed destined for the league's cellar. Yet, with a blend of strategic brilliance and sheer will, Bis orchestrated the most inspiring comeback season the league had ever witnessed. Week after week, his team clawed its way back, defying statistical probabilities and silencing doubters, ultimately culminating in a triumphant championship victory. It was a season that mirrored his own life's journey – a testament to never giving up, no matter how bleak the initial outlook. Today, Bis stands as a man who has truly cultivated his own garden. He is happily married, a devoted husband and a proud father to two wonderful children, building a life rich in love and accomplishment. His story is a powerful reminder that even from the hardest ground, a beautiful and unyielding bloom can emerge.",
+       "photo": "/managers/Bisacquino.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "phi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -43,10 +43,10 @@ export const managers = [
          link: "399301755705286656", // manager array number within this array, or null to link back to all managers page
          image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
        },
-       "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+       "favoritePlayer": 4144, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-       "philosophy": "Stay alive, even if you're 0-5.", // (optional)
+       "philosophy": "Trade Everyone.", // (optional)
        "tradingScale": 10, // 1 - 10 (optional)
        "preferredContact": null,  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
      }, 
