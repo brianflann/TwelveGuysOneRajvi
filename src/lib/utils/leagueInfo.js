5e40a39be40b246750b4f15c090661cae4b9cf6e
brianflann <bflannery7089@gmail.com>
2025-07-17 13:44:12 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -14,7 +14,7 @@ export const homepageText = `
   <p>As most of the league's members are part-time degenerate sports bettors, the victor's
   cash prize is lucrative indeed. However, the glory of hoisting the sacred Rajvi and relishing in her eternal beauty is a prize that transcends the social construct of monetary value.</p>
   <p>   </p>
-  <p>Long May She Reign.</p>
+  <p>                                                                                                       Long May She Reign.                                                         </p>
 `;
 
 /*   STEP 3   */
