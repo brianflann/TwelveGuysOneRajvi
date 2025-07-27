30a89b084453cfada35a26a5a78af3b3d53b9f73
brianflann <bflannery7089@gmail.com>
2025-07-27 14:52:19 -0400

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -169,18 +169,18 @@
     
         <h4 bind:this={oneOneOne}>1.1.1 Position Maximums</h4>
         
-        \\<div class="subBlock">
-            \\<table class="positionMaximums">
-                \\<tbody>
-                \\<tr><td>QB</td><td class="right">3 active, 5 total</td></tr>
-                \\<tr><td>RB</td><td class="right">20</td></tr>
-                \\<tr><td>WR</td><td class="right">20</td></tr>
-                \\<tr><td>TE</td><td class="right">10</td></tr>
-                \\<tr><td>D/ST</td><td class="right">3</td></tr>
-                \\<tr><td>K</td><td class="right">3</td></tr>
-                \\</tbody>
-            \\</table>
-        \\</div>
+        <div class="subBlock">
+            <table class="positionMaximums">
+                <tbody>
+                <tr><td>QB</td><td class="right">3 active, 5 total</td></tr>
+                <tr><td>RB</td><td class="right">20</td></tr>
+                <tr><td>WR</td><td class="right">20</td></tr>
+                <tr><td>TE</td><td class="right">10</td></tr>
+                <tr><td>D/ST</td><td class="right">3</td></tr>
+                <tr><td>K</td><td class="right">3</td></tr>
+                </tbody>
+            </table>
+        </div>
 
 
     <h3 class="subSectionHeading" bind:this={oneTwo}>1.2 Trading</h3>
