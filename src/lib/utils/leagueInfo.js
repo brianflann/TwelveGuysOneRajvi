26037ca25260078a176f411b8588661a9e508dd9
brianflann <bflannery7089@gmail.com>
2025-08-01 14:56:40 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -220,7 +220,7 @@ export const managers = [
        "bio": "<p></p>34 y/o he/him, born in Montgomeryville, PA. Spent 12 years in Philadelphia. Moved to Seattle, WA with partner Julia Canino in the summer of 2024. Currently a regional manager of the West coast team at a Pharmaceutical Equipment company. Enjoys cycling, hiking, cooking and hanging out with his dog. Has been to 48/50 states(nbd).",
        "photo": "/managers/Perri.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-       "favoriteTeam": "phi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+       "favoriteTeam": "dal", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
          name: "Eric Stoll", // Can be anything (usually your rival's name)
