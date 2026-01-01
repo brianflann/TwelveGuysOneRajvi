915fc93a3c02b16939e8a961651bc51c474130bc
brianflann <bflannery7089@gmail.com>
2026-01-01 12:12:20 -0500

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -168,8 +168,12 @@
         </ul>
         <li>Switching to .5 PPR (with the option of adding a fractional point for 1st downs)</li>
         <li>Changing the point calculation of QB's rushing & receiving yards to mirror their passing yards points (from 10yds to 20yds = 1 point) </li>
-        <li>Make Sunday morning drops immediate Free Agents</li>
+        <li>Turn off FA days for Thursday,Sunday and Monday in between draft day and start of season (everyday is waivers until Thursday week 1)</li>
+        <li>Make Sunday, Monday and Thursday drops immediate Free Agents</li>
+        <li>*If Sleeper settings allow* FAs are only players that are playing on that specific day (i.e only the Thursday night game players available on Thursdays)</li>
+        <li>Thursday FA window opens closer to kickoff rather than all day (i.e Wednesday waiver period is longer or there is a second shorter waiver during the day on Thursday)</li>
         <li>Eliminate points for special teams TDs</li>
+        <li>Keeper at ADP round week of the draft</li>
         <li>Managers receive a lottery ticket for every completed trade. A winning lottery ticket is chosen at the following year's draft. The prize is the weekly lowest points collection.</li>
         <li>If you make a trade, you get additional FAAB money. Amount to be decided at a later date.</li>
         <li>Drafting on Saturdays going forward</li>
